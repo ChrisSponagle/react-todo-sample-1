ca87bbce0ad16b9551d6e7d0be4e561f45d4c102
Chris Sponagle <Chriss.tools@keysupreme.com>
2020-03-04 17:13:10 -0400

    moved images and fonts into single assets folder

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './css/App.css';
 import { store } from './store.js';
 import Todos from './components/Todos';
 import { Provider } from 'react-redux';
-import './fonts/Roboto/Roboto-Regular.ttf';
+import './assets/fonts/Roboto/Roboto-Regular.ttf';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <div>
-          <img className='appLogo' role='presentation' src={require('./images/tranquilityLogo.png')} />
+          <img className='appLogo' role='presentation' src={require('./assets/images/tranquilityLogo.png')} />
           <h2 className='headerFont'>Tranquility<br/>ToDo App</h2>
         </div>
         <Provider store={store}>
